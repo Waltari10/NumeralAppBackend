e6d0749894ba3b483450f60be48c91f4e4df1ed3
valtteriL <valtteri.laine@takamaki.fi>
2019-09-25 19:17:14 +0300

    Adds basic server.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const romanToArabic = require('./roman')
+
+app.post('/', (req, res) => { 
+
+    const roman = req.query.roman;
+
+ 
+    // Cast number to string, otherwise express thinks we are sending a invalid status code.
+    res.send(romanToArabic(roman) + '');
+
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
